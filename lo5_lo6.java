847d6dc56bd8fe87d723c2d0dccbbb8c3791e8f2
NhatHuy <lenhathuy9a6@gmail.com>
2023-09-28 15:28:20 +0700

    baitap_LO5_LO6

---
+++ lo5_lo6.java
@@ -0,0 +1,15 @@
+package PRO192;
+
+import java.util.Scanner;
+
+public class lo5_lo6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Person x = new Person(sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextInt());
+        x.Display();
+        x.changediachi(sc.nextLine());
+        x.changediachi(sc.nextLine());
+        x.Display();
+    }
+
+}
